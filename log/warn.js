d44d6b324bd281f52cb30b6861d98b7a463280da
Pouya <pouya.eghbali@yandex.com>
2021-06-27 21:57:22 +0200

    Fix warning messages

--- log/warn.js
+++ log/warn.js
@@ -3,7 +3,6 @@ const chalk = require("chalk");
 
 const warn = ({ text, config }) => {
   message({ text: chalk.yellow(text), level: 2, config });
-  process.exit(1);
 };
 
 module.exports.warn = warn;
