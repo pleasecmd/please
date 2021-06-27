7c5b5f8abd541d749e02f4f759e5f6f607fc64fd
Pouya <pouya.eghbali@yandex.com>
2021-06-27 13:56:24 +0200

    Fix cli options parsing

--- cli/postprocess.js
+++ cli/postprocess.js
@@ -1,14 +1,20 @@
+const removeUndefined = (args) => {
+  const entries = Object.entries(args);
+  const filtered = entries.filter(([_, value]) => typeof value !== "undefined");
+  return Object.fromEntries(filtered);
+};
+
 const postprocess = (args) => {
   const processed = {};
-  processed.silent = !!args.silent;
-  processed.silentRun = processed.silent || !!args["silent-run"];
-  processed.silentInstall = processed.silent || !!args["silent-install"];
-  processed.verbose = !!args.verbose;
+  processed.silent = args.silent;
+  processed.silentRun = processed.silent || args["silent-run"];
+  processed.silentInstall = processed.silent || args["silent-install"];
+  processed.verbose = args.verbose;
   processed.update = !args["no-update"];
   if (processed.silent) processed.log = 0;
-  if (!Number.isNaN(parseInt(args.log))) processed.log = args.log;
+  processed.log = args.log;
   processed.command = args.command;
-  return processed;
+  return removeUndefined(processed);
 };
 
 module.exports.postprocess = postprocess;
--- config/postprocess.js
+++ config/postprocess.js
@@ -2,13 +2,13 @@ const { getLogLevel } = require("../log");
 
 const postprocess = (config) => {
   const processed = {};
-  processed.silent = config.silent;
+  processed.silent = !!config.silent;
   processed.silentRun = processed.silent || !!config.silentRun;
   processed.silentInstall = processed.silent || !!config.silentInstall;
-  processed.verbose = config.verbose;
-  processed.update = config.update;
+  processed.verbose = !!config.verbose;
+  processed.update = !!config.update;
   processed.preferred = config.preferred;
-  processed.useCNF = config.useCNF;
+  processed.useCNF = !!config.useCNF;
   processed.log = getLogLevel(config);
   return processed;
 };
