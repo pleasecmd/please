3a8699755ca3ea7ee36d251f47ecef1f3abff1d1
Pouya <pouya.eghbali@yandex.com>
2021-07-04 21:05:49 +0200

    Return spawned process

--- utils/spawn.js
+++ utils/spawn.js
@@ -2,7 +2,7 @@ const { spawnSync } = require("child_process");
 
 const spawn = (command, argv, config) => {
   const stdio = config.silentRun ? "ignore" : "inherit";
-  spawnSync(command, argv, { shell: true, stdio });
+  return spawnSync(command, argv, { shell: true, stdio });
 };
 
 module.exports.spawn = spawn;
